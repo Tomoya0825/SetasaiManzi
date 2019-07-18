3d4e46e8f0fba6fa61ec1395aed739d06510e215
Tomoya0825 <tomoya21900@gmail.com>
2019-07-18 20:51:08 +0900

    万治

--- sqltest.js
+++ sqltest.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     database: 'setasai'
 });
 
-var qrlist={};
+var qrlist={};  //連想配列  { 1 : tcu_Ichgokan } ({ index : QR名 })
 
 //gittestsuyami3
 
@@ -19,7 +19,7 @@ connection.query('SELECT * FROM qrtable;', (err, results)=>{
         for(let index in results){
             qrlist[results[index]['qrid']] = results[index]['qr_text'];
         }
-        console.log(qrlist);
+        console.log(qrlist['1']);
 
         //ここに処理かくか非同期のやつなんやかんや。
 
